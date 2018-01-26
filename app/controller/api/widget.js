7676c707d6d9fc5549087d8792055acec8f0029b
zhanyi <376771432@qq.com>
2018-01-26 09:47:01 +0800

    获取实例接口

--- app/controller/api/widget.js
+++ app/controller/api/widget.js
@@ -8,8 +8,10 @@ class WidgetController extends BaseController {
 
     const data = await service.widget.getWidget(this.ctx.params.id);
     if (data !== null) {
+      this.app.logger.info(`成功获取组件实例数据：[${this.ctx.params.id}][${data.name}]`);
       this.success(data);
     } else {
+      this.app.logger.info(`未找到组件实例数据：${this.ctx.params.id}`);
       this.notFound();
     }
   }
