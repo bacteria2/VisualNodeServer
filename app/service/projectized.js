7bd701196c87cd4474fcd37eb03c04989c7bfe7c
zhanyi <376771432@qq.com>
2018-02-09 10:26:09 +0800

    修改获取项目列表接口

--- app/service/projectized.js
+++ app/service/projectized.js
@@ -4,7 +4,9 @@ const Service = require('egg').Service;
 const ObjectID = require('mongodb').ObjectID;
 
 class ProjectizedService extends Service {
-  async list(query){
+  async queryMyProjects(){
+    //查询项目经理是当前用户或者项目成员包含当前用户的项目
+    const query = {$or:[{'projectManager.userid':this.ctx.user.userid},{'members.userid':this.ctx.user.userid}]};
     const collection = this.app.mongo.db.collection('projects');
     const projects = await collection.find(query).toArray();
     return projects;
