017854123e8801a94dc9989f433ed9c28731246a
shepard.xia <xia8910168@hotmail.com>
2018-03-02 15:36:32 +0800

    添加原型render脚本读取

--- app/service/prototypes.js
+++ app/service/prototypes.js
@@ -36,11 +36,11 @@ class PrototypeService extends Service {
 
   async getUiMeta(id) {
     const collection = this.app.mongo.db.collection(collectionName);
-    const metaDefine = await collection.find({ _id: ObjectId(id) }, { _id: 0, optionMeta: 1, dataMeta: 1 }).next();
-    const { optionMeta: { normal, addable },dataMeta } = metaDefine;
-    const normalTempales = await this.service.template.getTemplatesByNames(normal);
-    const addableTempales = await this.service.template.getTemplatesByNames(addable);
-    return { optionMeta: { normal: normalTempales, addable: addableTempales }, dataMeta };
+    const metaDefine = await collection.find({ _id: ObjectId(id) }, { _id: 0,render:1, optionMeta: 1, dataMeta: 1 }).next();
+    const { optionMeta: { normal, addable },...otherMeta } = metaDefine;
+    const normalTemplate = await this.service.template.getTemplatesByNames(normal);
+    const addableTemplate = await this.service.template.getTemplatesByNames(addable);
+    return { optionMeta: { normal: normalTemplate, addable: addableTemplate },...otherMeta};
   }
 
 }
